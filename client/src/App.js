45f8f0b212cbedad415817e58ddd52aeb9d488a4
Temitope Oyesiji <temitopeayomikun999@gmail.com>
2025-11-27 09:35:11 +0100

    Add client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
